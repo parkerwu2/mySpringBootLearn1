e49b45a12f03b3ac7021b0d8674a523a3b3663c5
parkerwu2 <982051137@qq.com>
2018-03-20 16:00:18 +0800

    update jms activemq

---
+++ src/main/java/com/wjz/springboot1/service/jms/Consumer.java
@@ -0,0 +1,16 @@
+package com.wjz.springboot1.service.jms;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jingzhi.wu on 2018/3/20.
+ */
+@Component
+public class Consumer {
+    // 使用JmsListener配置消费者监听的队列，其中text是接收到的消息
+    @JmsListener(destination = "mytest.queue")
+    public void receiveQueue(String text) {
+        System.out.println("Consumer收到的报文为:" + text);
+    }
+}
